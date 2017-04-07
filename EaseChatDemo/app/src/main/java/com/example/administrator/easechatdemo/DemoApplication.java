163595289b3b71cc101c7eff84bd58ce39827828
wangyong <874999821@qq.com>
2017-04-07 18:45:30 +0800

    提交demo

---
+++ EaseChatDemo/app/src/main/java/com/example/administrator/easechatdemo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.administrator.easechatdemo;
+
+import android.app.Application;
+
+import com.hyphenate.easeui.controller.EaseUI;
+
+public class DemoApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EaseUI.getInstance().init(this, null);
+    }
+    
+}
